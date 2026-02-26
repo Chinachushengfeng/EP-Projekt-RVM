43edf2ce3fc0ef0d0940b8a7ef603475b007f01c
Chu <shengfeng2015@163.com>
2026-02-26 13:59:27 +0800

    3333

--- js/login.js
+++
@@ -1,9 +0,0 @@
-function back() {
-    window.history.go(-1);
-}
-
-function continueNext() {
-    sessionStorage.url = "user.html";
-    window.location.href = "user.html";
-
-}
--- js/repair.js
+++
@@ -1,4 +0,0 @@
-function end() {
-    sessionStorage.url = "start.html";
-    window.location.href = "start.html";
-}
